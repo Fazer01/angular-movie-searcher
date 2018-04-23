6940ed0e1bbad641cb3b36d4b447c34e53f4895c
Tom <tom@tom.nl>
2018-04-24 00:02:20 +0200

    Additional changed
    Navigate from title to movie detail page

--- src/app/shared/movie.ts
+++ src/app/shared/movie.ts
@@ -1,25 +1,3 @@
-// export interface Movie {
-    
-//     // constructor(
-//     //     public vote_count: number,
-//     //     public id: number,
-//     //     public video: boolean,
-//     //     public vote_average: number,
-//     //     public title: string,
-//     //     public popularity: number,
-//     //     public poster_path: string,
-//     //     public original_language: string,
-//     //     public original_title: string,
-//     //     public genre_ids: number[],
-//     //     public backdrop_path: string,
-//     //     public adult: boolean,
-//     //     public overview: string,
-//     //     public release_date: string)
-//     //     {}
-    
-//     // constructor(public t1: string, public t2: string)
-//     // {}
-// }
 export interface Movie{
     vote_count: number;
     id: number;
